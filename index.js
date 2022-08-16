a2a5329747b9e6a518be05df2f0d141c43e57303
Gracias Kasongo <graciaskas96@gmail.com>
2022-08-16 19:39:57 +0200

    add files

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
 
-app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "views");
 
+app.use(express.static("public"));
 app.use(function(req,res){
     res.render("404");
 });
