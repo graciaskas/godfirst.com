2d4e37d9388150c12369a59be6ef7f308fe2ae71
Gracias Kasongo <graciaskas96@gmail.com>
2022-08-18 11:59:18 +0200

    Updated

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
+const ejs_layouts = require("express-ejs-layouts");
 const app = express();
 
-//base variables
+//PORT
 const PORT = process.env.port || 8072;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
 
+//Setup templating engine
+app.use(ejs_layouts);
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-app.use(express.static("public"));
+app.get("", function(req,res) {
+    res.render("index");
+})
+app.get("/about", function(req,res) {
+    res.render("about");
+})
+app.get("/contact", function(req,res) {
+    res.render("contact");
+})
+
+//Setup static files
+app.use(express.static(path.join(__dirname,"/public")));
+
 app.use(function(req,res){
     res.render("404");
 });
